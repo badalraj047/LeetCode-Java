99717e29b238a09f1a43c26a3ece5759f4080713
Badal Raj <114842576+badalraj047@users.noreply.github.com>
2025-02-11 00:18:22 +0530

    Create ToLowerCase.java
    LeetCode Problem - To Lower Case

---
+++ ToLowerCase.java
@@ -0,0 +1,37 @@
+// This Problem contains my solution to the [LeetCode "To Lower Case" problem] - https://leetcode.com/problems/to-lower-case/
+// To run this code in VS Code, you need to:
+// Add a main method to execute the code.
+// Ensure your Java setup is correct (JDK installed and configured).
+
+// 709. To Lower Case
+// Problem Statement:
+
+// Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+// Example 1:
+// Input: s = "Hello"
+// Output: "hello"
+
+// Example 2:
+// Input: s = "here"
+// Output: "here"
+
+// Example 3:
+// Input: s = "LOVELY"
+// Output: "lovely"
+
+
+// Constraints:
+
+// 1 <= s.length <= 100
+// s consists of printable ASCII characters.
+
+// Solution:
+
+class Solution {
+    public String toLowerCase(String s) {
+        String word = s.toLowerCase();
+        return word;
+    }
+}
+
