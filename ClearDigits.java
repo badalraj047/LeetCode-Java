706dd062c66dcbe18af47c7c781e1bb0d3926e57
Badal Raj <114842576+badalraj047@users.noreply.github.com>
2025-02-10 09:51:04 +0530

    Create ClearDigits.java
    LeetCode Problem - Clear Digits

---
+++ ClearDigits.java
@@ -0,0 +1,49 @@
+// This Problem contains my solution to the [LeetCode "Clear Digits" problem] - https://leetcode.com/problems/clear-digits/
+// To run this code in VS Code, you need to:
+// Add a main method to execute the code.
+// Ensure your Java setup is correct (JDK installed and configured).
+
+// 3174. Clear Digits
+// Problem Statement:
+
+// You are given a string s.
+// Your task is to remove all digits by doing this operation repeatedly:
+// Delete the first digit and the closest non-digit character to its left.
+// Return the resulting string after removing all digits.
+
+// Example 1:
+// Input: s = "abc"
+// Output: "abc"
+// Explanation:
+// There is no digit in the string.
+
+// Example 2:
+// Input: s = "cb34"
+// Output: ""
+// Explanation:
+// First, we apply the operation on s[2], and s becomes "c4".
+// Then we apply the operation on s[1], and s becomes "".
+
+// Constraints:
+
+// 1 <= s.length <= 100
+// s consists only of lowercase English letters and digits.
+// The input is generated such that it is possible to delete all digits.
+
+// Solution:
+
+class Solution {
+    public String clearDigits(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        for(int i=0; i<sb.length(); i++){
+            if(Character.isDigit(sb.charAt(i))){
+                sb.deleteCharAt(i);
+                sb.deleteCharAt(i-1);
+                i-=2;
+            }
+        }
+        return sb.toString();
+    }
+}
+
+
