79338f86262518fc525102a9c1bbc0daf19aaf9b
Badal Raj <114842576+badalraj047@users.noreply.github.com>
2025-02-11 08:50:16 +0530

    Update and rename CheckIftheSentenceIsPangram.java to Check if the Sentence Is Pangram.java
    LeetCode Problem - 1832. Check if the Sentence Is Pangram

--- Check if the Sentence Is Pangram.java
+++ Check if the Sentence Is Pangram.java
@@ -1,4 +1,4 @@
-// This Problem contains my solution to the [LeetCode "Check if the Sentence Is Pangram" problem] - https://leetcode.com/problems/check-if-the-sentence-is-pangram/description/
+// This Problem contains my solution to the [LeetCode "1832. Check if the Sentence Is Pangram" problem] - https://leetcode.com/problems/check-if-the-sentence-is-pangram/description/
 // To run this code in VS Code, you need to:
 // Add a main method to execute the code.
 // Ensure your Java setup is correct (JDK installed and configured).
