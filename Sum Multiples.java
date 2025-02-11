73ba1d920a6532387f16935ba1b9fe5b049e3687
Badal Raj <114842576+badalraj047@users.noreply.github.com>
2025-02-11 09:04:42 +0530

    Update and rename SumMultiples.java to Sum Multiples.java
    LeetCode Problem - 2652. Sum Multiples

--- Sum Multiples.java
+++ Sum Multiples.java
@@ -1,4 +1,4 @@
-// This Problem contains my solution to the [LeetCode Sum Multiples problem] - https://leetcode.com/problems/sum-multiples/
+// This Problem contains my solution to the [LeetCode "2652. Sum Multiples" problem] - https://leetcode.com/problems/sum-multiples/
 // To run this code in VS Code, you need to:
 // Add a main method to execute the code.
 // Ensure your Java setup is correct (JDK installed and configured).
