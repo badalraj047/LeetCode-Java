b1f800c937a2d0f6a4eae0d3bf1d38ac5701ee25
Badal Raj <114842576+badalraj047@users.noreply.github.com>
2025-02-11 08:55:59 +0530

    Update and rename DivisibleAndNon-divisibleSumsDifference,.java to Divisible and Non-divisible Sums Difference.java
    LeetCode Problem - 2894. Divisible and Non-divisible Sums Difference

--- Divisible and Non-divisible Sums Difference.java
+++ Divisible and Non-divisible Sums Difference.java
@@ -1,4 +1,4 @@
-// This Problem contains my solution to the [LeetCode "Divisible and Non-divisible Sums Difference" problem] - https://leetcode.com/problems/divisible-and-non-divisible-sums-difference/
+// This Problem contains my solution to the [LeetCode "2894. Divisible and Non-divisible Sums Difference" problem] - https://leetcode.com/problems/divisible-and-non-divisible-sums-difference/
 // To run this code in VS Code, you need to:
 // Add a main method to execute the code.
 // Ensure your Java setup is correct (JDK installed and configured).
