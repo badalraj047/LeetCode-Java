ef90bb3da4674f7e0ebaa4267d10b6c6f734c512
Badal Raj <114842576+badalraj047@users.noreply.github.com>
2025-02-11 08:53:09 +0530

    Update and rename CountNumberOfBadPairs.java to Count Number of Bad Pairs.java
    LeetCode Problem - 2364. Count Number of Bad Pairs

--- Count Number of Bad Pairs.java
+++ Count Number of Bad Pairs.java
@@ -1,4 +1,4 @@
-// This Problem contains my solution to the [LeetCode "Count Number of Bad Pairs" problem] - https://leetcode.com/problems/count-number-of-bad-pairs/
+// This Problem contains my solution to the [LeetCode "2364. Count Number of Bad Pairs" problem] - https://leetcode.com/problems/count-number-of-bad-pairs/
 // To run this code in VS Code, you need to:
 // Add a main method to execute the code.
 // Ensure your Java setup is correct (JDK installed and configured).
