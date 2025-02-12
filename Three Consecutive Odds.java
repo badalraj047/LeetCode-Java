903b97011b66f1dc4c25d35d312b6cb91c0289db
Badal Raj <114842576+badalraj047@users.noreply.github.com>
2025-02-12 20:27:20 +0530

    Create Three Consecutive Odds.java
    LeetCode Problem - 1550. Three Consecutive Odds

---
+++ Three Consecutive Odds.java
@@ -0,0 +1,46 @@
+// This Problem contains my solution to the [LeetCode "258. Add Digits" problem] - https://leetcode.com/problems/three-consecutive-odds/
+// To run this code in VS Code, you need to:
+// Add a main method to execute the code.
+// Ensure your Java setup is correct (JDK installed and configured).
+
+// 1550. Three Consecutive Odds
+// Problem Statement:
+
+// Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+ 
+// Example 1:
+// Input: arr = [2,6,4,1]
+// Output: false
+// Explanation: There are no three consecutive odds.
+
+// Example 2:
+// Input: arr = [1,2,34,3,4,5,7,23,12]
+// Output: true
+// Explanation: [5,7,23] are three consecutive odds.
+
+// Constraints:
+
+// 1 <= arr.length <= 1000
+// 1 <= arr[i] <= 1000
+
+// Solution:
+
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count = 0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] % 2 == 1){
+                count++;
+                if(count == 3){
+                    return true;
+                }
+            }else{
+                count = 0;
+            }
+            
+        }
+        return false;
+    }
+}
+
+
